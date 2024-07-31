ba19b8f05733dba23870d05168301ffc916d0182
Rami Alrabi <77370385+Strutio@users.noreply.github.com>
2024-07-31 18:49:44 -0400

    Add files via upload

---
+++ fitness-trainer/src/main/java/com/example/fitness_trainer/FitnessTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitness_trainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrainerApplication.class, args);
+	}
+
+}
---
+++ fitness-trainer/src/test/java/com/example/fitness_trainer/FitnessTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitness_trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
